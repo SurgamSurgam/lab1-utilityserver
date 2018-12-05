7c770f26d000051d8bb6da1dabdc25470a784871
Deyvi <deyvio@gmail.com>
2018-12-05 01:21:30 -0500

    saved with prettier

--- gif-search-index.js
+++ gif-search-index.js
@@ -1,29 +1,27 @@
-const express = require('express');
-const axios = require('axios')
+const express = require("express");
+const axios = require("axios");
 const app = express();
 const port = 3000;
 
-
-app.get('/gif', (req, res) => {
+app.get("/gif", (req, res) => {
   let query = req.query;
   let items = Object.values(query);
-  let apiKey = 'bh9XoPmSxq8G94wQUNX2vvTb4vuePoNo';
+  let apiKey = "bh9XoPmSxq8G94wQUNX2vvTb4vuePoNo";
   let url = `http://api.giphy.com/v1/gifs/search?q=${items[0]}&api_key=${apiKey}&limit=2`;
   let images = [];
 
   axios
-  .get(url)
-  .then(res => {
-    let arrObj = res.data.data;
-    arrObj.forEach(el => images.push(el.url));
-  })
-  .then(()=> res.send(images))
-  .catch(res => {
-    console.log('error', res);
-  })
+    .get(url)
+    .then(res => {
+      let arrObj = res.data.data;
+      arrObj.forEach(el => images.push(el.url));
+    })
+    .then(() => res.send(images))
+    .catch(res => {
+      console.log("error", res);
+    });
+});
 
-})
-
-app.listen(port, ()=> {
+app.listen(port, () => {
   console.log(`Listening to port ${port}`);
-})
+});
